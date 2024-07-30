add7f8db695d2bb34cf640fedce8f3ab87e23790
Clément Mathieu--Drif <cmdetu@gmail.com>
2024-07-30 21:46:37 +0200

    util: Fix and refactor cc list sanitization function
    All the addresses must be valid

--- patchr-app/crates/common/src/util/input.rs
+++ patchr-app/crates/common/src/util/input.rs
@@ -2,9 +2,7 @@ use email_address::EmailAddress;
 
 pub fn sanitize_cc_list(input: &str) -> Option<&str> {
     let value = input.trim();
-    let valid_input = value.split(",").any(|elt| {
-        EmailAddress::is_valid(elt)
-    });
+    let valid_input = value.split(",").all(EmailAddress::is_valid);
     if valid_input {
         Some(value)
     } else {
