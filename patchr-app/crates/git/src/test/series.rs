9de26498ad54e4cd073603418f13842a257208c1
Clément Mathieu--Drif <cmdetu@gmail.com>
2024-07-31 21:09:47 +0200

    test: Add tests for setters of the series struct

--- patchr-app/crates/git/src/test/series.rs
+++ patchr-app/crates/git/src/test/series.rs
@@ -36,3 +36,75 @@ fn test_series_revs() {
         series.delete_revision(2);
     }
 }
+
+#[test]
+fn test_series_name() {
+    let mut series = Series::new("name", "title").unwrap();
+
+    assert!(series.set_name("").is_err());
+    assert_eq!(series.name(), "name");
+    assert!(series.set_name("test$a").is_err());
+    assert!(series.set_name("test-a").is_ok());
+    assert_eq!(series.name(), "test-a");
+    assert!(series.set_name("a name").is_err());
+    assert!(series.set_name("a_-_name").is_ok());
+    assert!(series.set_title("aAgGdDpP---RTY").is_ok());
+    // TODO: add more tests
+}
+
+#[test]
+fn test_series_short_name() {
+    let mut series = Series::new("name", "title").unwrap();
+
+    assert_eq!(series.short_name(), "");
+    assert!(series.set_short_name("a").is_ok());
+    assert_eq!(series.short_name(), "a");
+
+    assert!(series.set_short_name("a space").is_err());
+
+    assert!(series.set_short_name("not-a-space").is_ok());
+    assert_eq!(series.short_name(), "not-a-space");
+
+    assert!(series.set_short_name("not_a_space").is_ok());
+    assert_eq!(series.short_name(), "not_a_space");
+
+    assert!(series.set_short_name("not+allowed").is_err());
+    assert!(series.set_short_name("fancy-name-that-is-not-so-short").is_err());
+
+    assert!(series.set_short_name("").is_ok());
+    assert_eq!(series.short_name(), "");
+    // TODO: add more test
+}
+
+#[test]
+fn test_series_title() {
+    let mut series = Series::new("name", "title").unwrap();
+
+    assert!(series.set_title("").is_err());
+    assert_eq!(series.title(), "title");
+    assert!(series.set_title("test$a").is_err());
+    assert!(series.set_title("test-a").is_ok());
+    assert_eq!(series.title(), "test-a");
+    assert!(series.set_title("a title").is_ok());
+    assert!(series.set_title("a_-title").is_ok());
+    assert!(series.set_title("aAgGdDpP---RTY").is_ok());
+    // TODO: add more tests
+}
+
+#[test]
+fn test_series_cover_letter() {
+    let mut series = Series::new("name", "title").unwrap();
+
+    assert!(series.set_cover_letter("").is_ok());
+    assert_eq!(series.cover_letter(), "");
+    let cv = "test\n\n\t\na multi line \ncover\nletter";
+    assert!(series.set_cover_letter(cv).is_ok());
+    assert_eq!(series.cover_letter(), cv);
+
+    let cv = "a".repeat(1500);
+    assert!(series.set_cover_letter(cv.as_str()).is_ok());
+    assert_eq!(series.cover_letter(), cv);
+
+    assert!(series.set_cover_letter("").is_ok());
+    assert_eq!(series.cover_letter(), "");
+}
