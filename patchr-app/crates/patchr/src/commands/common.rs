562e5f96be786763e0d0ee49e84a1940c26d75d2
Clément Mathieu--Drif <cmdetu@gmail.com>
2024-08-04 13:37:31 +0200

    misc: Allow users to pass arguments to their default editor

--- patchr-app/crates/patchr/src/commands/common.rs
+++ patchr-app/crates/patchr/src/commands/common.rs
@@ -7,19 +7,34 @@ use crate::{
     user_data::{root_file::UserConfig, user_data::new_root_tmp_child_path},
 };
 
+fn extract_editor_args(editor: &str) -> Option<(String, Vec<String>)> {
+    let mut res = shlex::split(editor)?;
+    if res.len() < 1 {
+        return None;
+    }
+    let editor_bin = res.remove(0); // res.len() >= 1
+    Some((editor_bin, res))
+}
+
 pub fn edit_in_text_editor(user_config: &UserConfig, content: &str) -> Option<String> {
     debug!("Edit series");
     let Some(editor) = user_config.editor() else {
         cli_print_error!("Please define an editor");
         return None;
     };
-    let editor = String::from(editor);
+    let Some((editor, editor_args)) = extract_editor_args(editor) else {
+        cli_print_error!("Invalid editor configuration");
+        return None;
+    };
     let path = new_root_tmp_child_path().ok()?;
     if let Err(e) = fs::write(&path, content) {
         cli_print_error!("Cannot edit the content: {}", e.to_string());
         return None;
     };
-    let editor_res = process::Command::new(editor).arg(&path).status();
+    let editor_res = process::Command::new(editor)
+        .args(editor_args)
+        .arg(&path)
+        .status();
     if let Err(e) = editor_res {
         cli_print_error!("Editor process returned an error: {}", e.to_string());
         let _ = fs::remove_file(&path); // at least we tried
