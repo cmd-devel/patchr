cmd-devel/patchr

commit a783a72463f175ca56b5d31be15f3ca527636804

Initial release

patchr-app/crates/patchr/src/user_data/root_file.rs
@@ -1,0 +1,156 @@
+use common::constants::PROJECT_VERSION;
+use git::{repo::RepoMetadata, util::find_repo_root};
+use serde::{Deserialize, Serialize};
+
+use super::user_data::{UserDataError, UserDataErrorCode};
+
+pub const ROOT_FILE_NAME: &str = "root.json";
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct UserConfig {
+    editor: Option<String>,
+    send_command: Option<String>,
+    from_email: Option<String>,
+    smtp_server: Option<String>,
+    smtp_user: Option<String>,
+    smtp_port: Option<u16>,
+    smtp_encryption: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RootFile {
+    version: String,
+    user_config: UserConfig,
+    repos: Vec<RepoMetadata>,
+}
+
+impl UserConfig {
+    pub fn new() -> Self {
+        Self {
+            editor: None,
+            send_command: None,
+            from_email: None,
+            smtp_server: None,
+            smtp_user: None,
+            smtp_port: None,
+            smtp_encryption: None,
+        }
+    }
+
+    pub fn editor(&self) -> Option<&str> {
+        self.editor.as_ref().map(String::as_str)
+    }
+
+    pub fn set_editor(&mut self, editor: &str) {
+        self.editor = Some(String::from(editor));
+    }
+
+    pub fn send_command(&self) -> Option<&str> {
+        self.send_command.as_ref().map(String::as_str)
+    }
+
+    pub fn set_send_command(&mut self, send_command: Option<&str>) {
+        self.send_command = send_command.map(String::from);
+    }
+
+    pub fn from_email(&self) -> Option<&str> {
+        self.from_email.as_ref().map(String::as_str)
+    }
+
+    pub fn set_from_email(&mut self, from_email: &str) {
+        self.from_email = Some(String::from(from_email));
+    }
+
+    pub fn smtp_server(&self) -> Option<&str> {
+        self.smtp_server.as_ref().map(String::as_str)
+    }
+
+    pub fn set_smtp_server(&mut self, smtp_server: Option<&str>) {
+        self.smtp_server = smtp_server.map(String::from);
+    }
+    
+    pub fn smtp_user(&self) -> Option<&str> {
+        self.smtp_user.as_ref().map(String::as_str)
+    }
+    
+    pub fn set_smtp_user(&mut self, smtp_user: Option<&str>) {
+        self.smtp_user = smtp_user.map(String::from);
+    }
+
+    pub fn smtp_port(&self) -> Option<u16> {
+        self.smtp_port
+    }
+
+    pub fn set_smtp_port(&mut self, smtp_port: Option<u16>) {
+        self.smtp_port = smtp_port;
+    }
+
+    pub fn smtp_encryption(&self) -> Option<&str> {
+        self.smtp_encryption.as_ref().map(String::as_str)
+    }
+
+    pub fn set_smtp_encryption(&mut self, smtp_encryption: Option<&str>) {
+        self.smtp_encryption = smtp_encryption.map(String::from);
+    }
+}
+
+impl RootFile {
+    pub fn new() -> Self {
+        Self {
+            version: String::from(PROJECT_VERSION),
+            user_config: UserConfig::new(),
+            repos: Vec::new(),
+        }
+    }
+
+    pub fn find_repo_by_path(&self, path: &str) -> Option<RepoMetadata> {
+        self.repos.iter().find(|r| r.path() == path).cloned()
+    }
+
+    // TODO: use a hash table instead?
+    // The number of repos should not make the lookup slow
+    fn repo_exists(&self, name: &str, path: &str) -> bool {
+        self.repos
+            .iter()
+            .find(|r| r.name() == name || r.path() == path)
+            .is_some()
+    }
+
+    pub fn register_repo(
+        &mut self, name: &str, path: &str,
+    ) -> Result<&RepoMetadata, UserDataError> {
+        if self.repo_exists(name, path) {
+            return Err(UserDataError::new(UserDataErrorCode::RepoAlreadyExists));
+        }
+        let Some(path) = find_repo_root(path) else {
+            return Err(UserDataError::new(UserDataErrorCode::NotAGitRepo));
+        };
+        let repo = RepoMetadata::new(name, path.to_string_lossy().to_string().as_str());
+        self.repos.push(repo);
+        Ok(self.repos.last().unwrap())
+    }
+
+    pub fn delete_repo(&mut self, name: &str) -> Result<(), UserDataError> {
+        let count = self.repos.len();
+        self.repos.retain(|r| r.name() != name);
+        if count == self.repos.len() {
+            return Err(UserDataError::new_with_message(
+                UserDataErrorCode::RepoDoesNotExist,
+                format!("The repo named '{}' is not known", name),
+            ));
+        }
+        Ok(())
+    }
+
+    pub fn repos(&self) -> &[RepoMetadata] {
+        self.repos.as_slice()
+    }
+
+    pub fn config(&self) -> &UserConfig {
+        &self.user_config
+    }
+
+    pub fn config_mut(&mut self) -> &mut UserConfig {
+        &mut self.user_config
+    }
+}

.github/dependabot.yml
@@ -1,0 +1,12 @@
+# To get started with Dependabot version updates, you'll need to specify which
+# package ecosystems to update and where the package manifests are located.
+# Please see the documentation for more information:
+# https://docs.github.com/github/administering-a-repository/configuration-options-for-dependency-updates
+# https://containers.dev/guide/dependabot
+
+version: 2
+updates:
+ - package-ecosystem: "devcontainers"
+   directory: "/"
+   schedule:
+     interval: weekly

.rustfmt.toml
@@ -1,0 +1,5 @@
+max_width = 100
+fn_call_width = 80
+fn_params_layout = "Compressed"
+reorder_imports = true
+reorder_modules = true

patchr-app/.gitignore
@@ -1,0 +1,1 @@
+/target

patchr-app/Cargo.toml
@@ -1,0 +1,6 @@
+[workspace]
+members = [
+    "crates/*"
+]
+
+resolver = "2"

patchr-app/crates/common/Cargo.toml
@@ -1,0 +1,10 @@
+[package]
+name = "common"
+version = "0.1.0"
+edition = "2021"
+
+# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+
+[dependencies]
+lazy_static = "1.4.0"
+regex = "1.10.4"

patchr-app/crates/common/src/constants.rs
@@ -1,0 +1,4 @@
+pub const PROJECT_VERSION: &str = "0.12";
+pub const PROJECT_NAME: &str = "patchr";
+
+pub const GIT_COMMAND: &str = "git";

patchr-app/crates/common/src/lib.rs
@@ -1,0 +1,2 @@
+pub mod constants;
+pub mod util;

commit 373fcb39fd27c2f11e19426ea14fe03e3263cb39

user_data: Fix, find the root directory of a repo before checking if it exists
When registering a new repo, patchr checks whether this repo is
already registered or not. Moreover, it automatically finds the root
directory and uses it as the repo's "path".

These 2 operations must be swapped to make the duplicate
detection correct.

Fixes: a783a72463f1 (Initial release)

patchr-app/crates/patchr/src/user_data/root_file.rs
@@ -137,12 +137,12 @@ impl RootFile {
     pub fn register_repo(
         &mut self, name: &str, path: &str,
     ) -> Result<&RepoMetadata, UserDataError> {
-        if self.repo_exists(name, path) {
-            return Err(UserDataError::new(UserDataErrorCode::RepoAlreadyExists));
-        }
         let Some(path) = find_repo_root(path) else {
             return Err(UserDataError::new(UserDataErrorCode::NotAGitRepo));
         };
+        if self.repo_exists(name, path.to_string_lossy().to_string().as_str()) {
+            return Err(UserDataError::new(UserDataErrorCode::RepoAlreadyExists));
+        }
         let repo = RepoMetadata::new(name, path.to_string_lossy().to_string().as_str());
         self.repos.push(repo);
         Ok(self.repos.last().unwrap())